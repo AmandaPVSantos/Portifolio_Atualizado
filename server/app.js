e5a92b72acb2f78e293a8216050f0d8e2c9b7697
Amanda Santos <98029661+Asantosvp@users.noreply.github.com>
2024-04-30 23:01:09 -0300

    alterações

--- server/app.js
+++ server/app.js
@@ -1,18 +1,6 @@
-import express from 'express';
-import nodemailer from 'nodemailer';
-import cors from 'cors'; // Importe o pacote cors
-
-const app = express();
-const port = process.env.PORT || 3000; // Porta do servidor
-
-// Middleware para lidar com dados JSON
-app.use(express.json());
-app.use(cors()); // Use o middleware de CORS
-
-// Rota para receber dados do formulário e enviar e-mail
 app.post('/enviar-email', (req, res) => {
     // Extrair os dados do formulário do corpo da solicitação
-    const { nome, email, mensagem } = req.body;
+    const { nome, email, telefone, preferenciaContato, mensagem } = req.body;
 
     // Configurar o transporte de e-mail usando Nodemailer
     let transporter = nodemailer.createTransport({
@@ -28,23 +16,17 @@ app.post('/enviar-email', (req, res) => {
         from: email,
         to: 'amandasantosdev859@gmail.com',
         subject: 'Novo contato recebido',
-        text: `Nome: ${nome}\nEmail: ${email}\nMensagem: ${mensagem}`
+        text: `Nome: ${nome}\nEmail: ${email}\nTelefone: ${telefone}\nPreferência de contato: ${preferenciaContato}\nMensagem: ${mensagem}`
     };
 
     // Enviar e-mail
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.error('Erro ao enviar e-mail:', error);
-            console.log("mensagem teste");
             res.status(500).send('Erro ao enviar e-mail.');
         } else {
             console.log('E-mail enviado:', info.response);
             res.status(200).send('E-mail enviado com sucesso!');
         }
     });
-});
-
-// Iniciar o servidor
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
 });
